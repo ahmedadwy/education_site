829986b3e4c04e449b0abf66398d65a09be925e2
ahmedadwy <ahmedadwy200@gmail.com>
2024-02-15 14:08:41 +0200

    fourth commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,53 @@
 let menuBtn = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .flex .navbar');
 
-menuBtn.onclick = () =>{
+menuBtn.onclick = () => {
     menuBtn.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+
+
+var swiper = new Swiper(".course-slider", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+
+var swiper = new Swiper(".teachers-slider", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
